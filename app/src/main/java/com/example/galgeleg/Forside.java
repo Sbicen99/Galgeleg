31bd16102cf2f033928eb700117caaaffecbcfaf
Sbicen99 <sercanbicen99@gmail.com>
2019-09-30 17:54:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/galgeleg/Forside.java
@@ -0,0 +1,34 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Forside extends AppCompatActivity implements View.OnClickListener {
+
+Button minKnap1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        minKnap1 = findViewById(R.id.minKnap1);
+
+        minKnap1.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i = new Intent(this, Galgeleg_spillet.class);
+        startActivity(i);
+
+    }
+}
---
+++ app/src/main/java/com/example/galgeleg/Galgeleg_spillet.java
@@ -0,0 +1,20 @@
+package com.example.galgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Galgeleg_spillet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galgeleg_spillet);
+
+
+
+
+
+
+    }
+}
